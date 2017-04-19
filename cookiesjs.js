ee9d10fdb3e2ec0bfa8274033ef045520e8a92ab
Marcela Pino <marcelap99@aol.com>
2017-04-18 21:49:25 -0400

    resubmit hw not working

--- cookiesjs.js
+++ cookiesjs.js
@@ -1,60 +1,65 @@
+
 // SUGAR COOKIE
 var sugarPicture = document.querySelector("#picture1");
-var sugarClicks = 0;
-var sugarCount = document.querySelector("#count1");
+var sugarClicks = Cookies.get("sugarCookie") || 0
+var sugarCount1 = document.querySelector("#count1");
 sugarPicture.addEventListener("click", function(){
   sugarClicks++
+  // console.log(Cookies.get("sugarCookie"))
   Cookies.set("sugarCookie", sugarClicks)
+  sugarCount1.innerHTML = Cookies.get("sugarCookie")
 })
+
 function sugar(){
-sugarCount.innerHTML = Cookies.get("sugarCookie")
+  sugarCount1.innerHTML = Cookies.get("sugarCookie")
 }
 
 // CHOCOLATE COOKIE
 var chocolatePicture = document.querySelector("#picture2");
-var deleteButton2 = document.querySelector("#delete2");
-var sugarClicks2 = 0;
-var sugarCount2 = document.querySelector("#count2");
+var chocolateClicks = Cookies.get("chocolateCookie") || 0
+var chocolateCount2 = document.querySelector("#count2");
+
 chocolatePicture.addEventListener("click", function(){
-  sugarClicks2++
-  Cookies.set("chocolateCookie", sugarClicks2)
+  chocolateClicks ++
+  // console.log(Cookies.get("chocolateCookie"))
+  Cookies.set("chocolateCookie", chocolateClicks)
+  chocolateCount2.innerHTML = Cookies.get("chocolateCookie")
 })
+
   function chocolate(){
-  sugarCount2.innerHTML = Cookies.get("chocolateCookie")
+    chocolateCount2.innerHTML = Cookies.get("chocolateCookie")
 }
 
 // LEMON COOKIE
 var lemonPicture = document.querySelector("#picture3");
-var sugarClicks3 = 0;
+var lemonClicks = Cookies.get("lemonCookie") || 0
 var sugarCount3 = document.querySelector("#count3");
 
 lemonPicture.addEventListener("click",function(){
-  sugarClicks3++
-  Cookies.set("lemonCookie", sugarClicks3)
+  lemonClicks ++
+  // console.log(Cookies.get("lemonCookie"))
+  Cookies.set("lemonCookie", lemonClicks)
+  sugarCount3.innerHTML = Cookies.get("lemonCookie")
 })
-  function lemon(){
+
+function lemon(){
   sugarCount3.innerHTML = Cookies.get("lemonCookie")
 }
+
+
+
 
 // DELETE BUTTON
 var deleteButton = document.querySelector("#DELETE");
 var deleteClicks = 0;
 deleteButton.addEventListener("click",function(){
   Cookies.set("sugarCookie", 0);
-})
-var deleteButton = document.querySelector("#DELETE");
-var deleteClicks = 0;
-deleteButton.addEventListener("click",function(){
   Cookies.set("chocolateCookie", 0);
-})
-var deleteButton = document.querySelector("#DELETE");
-var deleteClicks = 0;
-deleteButton.addEventListener("click",function(){
   Cookies.set("lemonCookie", 0);
 })
 
 
 // SHOWS DATA STORED ON NEW ONLOAD
 window.onload = sugar()
-window.onlod = chocolate()
+window.onload = chocolate()
 window.onload = lemon()
